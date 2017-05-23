790f32212bc76e2f7316c17a8fcf39bc8fc31404
Max Horstmann <mhorstmann@stackoverflow.com>
2017-05-22 21:22:27 -0400

    sample lambda

---
+++ src/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Newtonsoft.Json;
+using Amazon.Lambda.Core;
+
+
+[assembly: LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace MyLambda
+{
+    public class Class1
+    {
+    	public object MyFunction()
+    	{
+    		return new { status = "OK" };
+    	}
+    }
+}
